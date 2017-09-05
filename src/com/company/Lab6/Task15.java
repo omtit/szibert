27cbfb803523c21823840b650caa9091acdf53ce
szibert1993 <sereganesrock@gmail.com>
2017-09-05 13:26:15 +0600

    Lab6: 15

--- src/com/company/Lab6/Task15.java
+++ src/com/company/Lab6/Task15.java
@@ -5,5 +5,15 @@ import java.util.Scanner;
 public class Task15 {
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
+        double a,b,r;
+        a=20;
+        b=40;
+        r=1;
+        while (a<=b) {
+            System.out.println(a);
+            r=r+Math.pow(a,3);
+            a=a+1;
+            System.out.print(r+" ");
+        }
     }
 }
